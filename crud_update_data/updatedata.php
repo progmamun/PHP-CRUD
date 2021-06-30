9fe1aaba5751af67d0987488a988d02ca18b594d
progmamun <mamun333089@gmail.com>
2021-06-30 11:03:59 +0600

    crud_update_data

---
+++ crud_update_data/updatedata.php
@@ -0,0 +1,16 @@
+<?php 
+$stu_id = $_POST['sid'];
+$stu_name = $_POST['sname'];
+$stu_address = $_POST['saddress'];
+$stu_class = $_POST['sclass'];
+$stu_phone = $_POST['sphone'];
+
+$conn = mysqli_connect("localhost","root","","crud") or die("Connection Faild");
+
+$sql = "UPDATE student SET sname = '{$stu_name}', saddress = '{$stu_address}', sclass = '{$stu_class}', sphone = '{$stu_phone}', WHERE sid = {$stu_id}";
+$result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+
+header("Location: http://localhost/crud/crud_create_data/index.php");
+
+mysqli_close($conn);
+?>
