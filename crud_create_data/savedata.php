7aaef8b115f00b50a40913ad76742b654e55f515
mamunrub <mamun333089@outlook.com>
2021-06-30 09:12:33 +0600

    edit-url

--- crud_create_data/savedata.php
+++ crud_create_data/savedata.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect("localhost","root","","crud") or die("Connection Faild");
 $sql = "INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
-header("Location: http://localhost/crud/index.php");
+header("Location: http://localhost/crud/crud_create_data/index.php");
 
 mysqli_close($conn);
 ?>
